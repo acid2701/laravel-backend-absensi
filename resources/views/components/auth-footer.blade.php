5b7d1d5539db8c2c9b0e76819777ab5c71e99c2b
acid2701 <caturhasbi@gmail.com>
2024-04-25 20:14:47 +0700

    fortify, login , logout

--- resources/views/components/auth-footer.blade.php
+++ resources/views/components/auth-footer.blade.php
@@ -1,3 +1,3 @@
   <div class="simple-footer">
-      Copyright &copy; Stisla 2018
+      Copyright &copy; Acid27
   </div>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard', ['type_menu' => 'dashboard']);
-});
-
-Route::get('/login', function () {
     return view('pages.auth.auth-login');
+});
+
+
+route::middleware(['auth'])->group(function(){
+    route::get('home', function(){
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
 });
